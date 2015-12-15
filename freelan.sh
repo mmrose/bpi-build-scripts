4bbca5ee872d1185046a8e9d385bc39477feb56d
Martin Mrose <github@penthranet.de>
2015-12-15 12:54:30 +0000

    Added freelan compilation script

---
+++ freelan.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+VERSION=2.0
+
+# install dependencies
+sudo aptitude install scons python libssl-dev libcurl4-openssl-dev libboost-system-dev \
+    libboost-thread-dev libboost-program-options-dev libboost-filesystem-dev \
+    libboost-iostreams-dev
+
+cd /storage/temp
+curl -L https://github.com/freelan-developers/freelan/archive/$VERSION.tar.gz | tar xz
+
+cd freelan-$VERSION
+
+# build
+scons
+
+# install
+sudo mv /opt/bin/freelan /opt/bin/freelan.old
+sudo cp /storage/temp/freelan-$VERSION/install/bin/freelan /opt/bin/freelan
